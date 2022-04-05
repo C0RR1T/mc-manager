9dd62e48d3804ac4056729785cf356d5b94428d9
CORR1T <corsin@ragett.li>
2022-04-05 13:27:00 +0200

    App

--- nest-backend/src/app.module.ts
+++ nest-backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServerModule } from './server/server.module';
+import { CommandlineService } from './commandline/commandline.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [ServerModule],
+    controllers: [AppController],
+    providers: [AppService, CommandlineService],
 })
 export class AppModule {}
