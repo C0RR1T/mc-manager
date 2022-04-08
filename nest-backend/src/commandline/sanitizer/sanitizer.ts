57fb5e43f02096eee21495c9643558b54c88ae64
Corsin Ragettli <corsin@ragett.li>
2022-04-08 18:35:34 +0200

    Sanitizer (going to be refactored)

--- nest-backend/src/commandline/sanitizer/sanitizer.ts
+++ nest-backend/src/commandline/sanitizer/sanitizer.ts
@@ -3,11 +3,19 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class Sanitizer {
     // TODO: Reimplement this
-    sanitizeInput(...args: string[]): string[] {
+    public sanitizeInput(...args: string[]): string[] {
         if (process.platform === 'win32') {
-            return args.map(v => v);
+            return args.map(i =>
+                i.split('').map(this.sanitizeWindowsInput).join('')
+            );
         } else {
             return args.map(v => v);
         }
     }
+
+    private sanitizeWindowsInput = (i: string): string =>
+        (['&', '\\', '<', '>', '^', '|', '"'].includes(i) ? '^' : '') +
+        this.sanitizePercent(i);
+
+    private sanitizePercent = (i: string): string => (i === '%' ? '%%' : i);
 }
